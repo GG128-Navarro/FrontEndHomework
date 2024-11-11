35a439b06df7c2aa0fbfacef7e8c1714417a747e
GG128-Navarro <navarrogonzalo128@gmail.com>
2024-11-11 16:48:52 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+function inclusive()
+   {
+document.body.style.backgroundImage = 'none';
+document.body.style.backgroundColor = "black";
+document.getElementById("titulo").style.color = "white";
+    }
+function creator()
+{alert("Sitio creado por Gonzalo Mateo Navarro. navarrogonzalo128@gmail.com");}
